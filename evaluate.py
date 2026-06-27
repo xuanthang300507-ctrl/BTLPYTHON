533f8a2deaea9dbb34ad9f23a1af6eeb6cffea01
xuanthang300507-ctrl <xuanthang300507@gmail.com>
2026-06-27 23:27:33 +0700

    hoan thien mo hinh

--- evaluate.py
+++ evaluate.py
@@ -1,10 +1,8 @@
-# evaluate.py
 from ultralytics import YOLO
-
-model = YOLO("runs/anpr_v1/weights/best.pt")
+model = YOLO("LAPTRINH PYTHON BTL/Dữ liệu train sau khi scale khuônn ảnh/weights/best.pt")
 
 # Đánh giá trên tập val
-metrics = model.val(data="data.yaml")
+metrics = model.val(data="LAPTRINH PYTHON BTL/vietnamese-license-plate-1/data.yaml")
 print(f"mAP@0.5:     {metrics.box.map50:.4f}")   # mục tiêu > 0.85
 print(f"mAP@0.5:0.95:{metrics.box.map:.4f}")
 print(f"Precision:   {metrics.box.mp:.4f}")
